2533b50706c6dafba92eca1828fa5487ef464f72
areddy14 <areddy1441@gmail.com>
2014-10-21 15:05:40 -0400

    I've added a code to tap into the petfinder api in the pet model.

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,7 @@
 class Pet < ActiveRecord::Base
-	
+	Petfinder.configure do |config|
+		config.api_key = ENV['PET_FINDER_KEY']
+		config.api_secret = ENV['PET_FINDER_SECRET']
+	end
+	petfinder = Petfinder::Client.new
 end

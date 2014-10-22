7a2afd9548b04139347dec35a592ed6e960c3a03
areddy14 <areddy1441@gmail.com>
2014-10-22 12:01:38 -0400

    I've added a realization to my migration for pet. You don't always need a database for an app. Usually you do though.

--- db/migrate/20141021175103_create_pets.rb
+++ db/migrate/20141021175103_create_pets.rb
@@ -1,5 +1,5 @@
 class CreatePets < ActiveRecord::Migration
-  def change
+  def change#Does this even matter? was able to get name for a pet without it being part of this, couldn't get breed when I tried. Was it necessary for this to exist in the database at all? It doesn't really seem that way, I just need to query petfinder's database and display the data. Properly built all the way through I'd probably need a different model though, for user.
     create_table :pets do |t|
       t.string :animal
       t.string :breed

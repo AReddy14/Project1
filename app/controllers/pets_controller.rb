dc4857b0cd1d7597e3ed2a59a850be49dfc23625
areddy14 <areddy1441@gmail.com>
2014-10-22 10:58:59 -0400

    Refactored index action in Pets controller, David

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,20 @@
 class PetsController < ApplicationController
 	def index
-		@pets = Pet.all#might need to switch this with to a call like what is in the gem, petfinder.find_pets(); may even want to avoid having anything in here so that I can go to the next stage with this empty before 
+		# These come from the URL
+		# Example: http://localhost:3000/pets?location=Boston,MA&kind=cat
+		location = params[:location]
+		kind = params[:kind]
+
+		if !location.nil? && !kind.nil?
+			# Search Petfinder
+			Pet.search(kind, location)
+		else
+			# Return an empty array of pets so that it doesn't get a NoMethod error when calling .each
+			@pets = []
+		end
+		#might need to switch this with to a call like what is in the gem, petfinder.find_pets(); may even want to avoid having anything in here so that I can go to the next stage with this empty before 
 	end
-	
+	# def show
+		
+	# end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,9 +1,15 @@
 class Pet < ActiveRecord::Base
+	KINDS = ['cat', 'dog', 'bird']
+
 	Petfinder.configure do |config|
 		config.api_key = ENV['PET_FINDER_KEY']
 		config.api_secret = ENV['PET_FINDER_SECRET']
 	end
-	petfinder = Petfinder::Client.new
+	@@petfinder = Petfinder::Client.new
 
-	
+	def self.search(animal, location)
+		# Returns XML
+		# Example: Pet.search('cat', 'Boston, MA')
+		@@petfinder.find_pets(animal, location)
+	end
 end

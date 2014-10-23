94a795361b7faf0e6de5808cc8140c0552731628
areddy14 <areddy1441@gmail.com>
2014-10-23 10:51:44 -0400

    I've run rake assets:precompile on suggestion by Shayna to fix my Heroku problems.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,10 +7,16 @@ class PetsController < ApplicationController
 		# binding.pry
 		if !location.nil? && !animal.nil?
 			# Search Petfinder
+			@shelter_ids = []
+
 			@pets = Pet.search(animal, location)
+			@pets.each do |pet|
+				@shelter_ids << pet.shelter_id
+			end
 		else
 			# Return an empty array of pets so that it doesn't get a NoMethod error when calling .each
 			@pets = []
+			@shelter_ids = []#may want to get rid of all the shelter_ids in index and change the location to somewhere better.
 		end
 		#might need to switch this with to a call like what is in the gem, petfinder.find_pets(); may even want to avoid having anything in here so that I can go to the next stage with this empty before 
 	end

771e36da35ddc61d8f2640637f9a4c6515d49110
areddy14 <areddy1441@gmail.com>
2014-11-20 19:43:55 -0500

    I forgot to add commits for a while. I've managed to fix the linking problem so I can get to individual pet pages. Pictures don't work yet, though I am only attempting to display a single photo for ever pet listed by a search. I am attempting to and failing to access the records for a single pet though at the moment. I was attempting to search the provided list by id and then only display data from that entry on the show page.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -9,18 +9,25 @@ class PetsController < ApplicationController
 			# Search Petfinder
 			@shelter_ids = []
 
-			@pets = Pet.search(animal, location)
-			@pets.each do |pet|
+			@pet_list = Pet.search(animal, location)
+			@pet_list.each do |pet|
 				@shelter_ids << pet.shelter_id
 			end
 		else
 			# Return an empty array of pets so that it doesn't get a NoMethod error when calling .each
-			@pets = []
+			@pet_list = []
 			@shelter_ids = []#may want to get rid of all the shelter_ids in index and change the location to somewhere better.
 		end
 		#might need to switch this with to a call like what is in the gem, petfinder.find_pets(); may even want to avoid having anything in here so that I can go to the next stage with this empty before 
 	end
 	def show
-		
+		# @pet = Pet.new(pet_params)
+		# animal_id = params[:]
+		@animal_id = params[:id] #successfully grabs the id for the animal I'm looking at. 
+		@pet = @pet_list.find(@animal_id)
+	end
+
+	def pet_params
+		params.require(:pet).permit(:name, :age, :id, :shelter_id)
 	end
 end
